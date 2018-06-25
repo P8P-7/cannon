f0d66fb58a0c5720485c4b6320492b5df7447114
Amorandron <jan@janwilts.com>
2018-06-25 15:04:04 +0200

    Fix timing

--- cannon/cannon.ino
+++ cannon/cannon.ino
@@ -6,26 +6,26 @@
 #define PIN_COIL 0x06
 #define PIN_VALVE_BUTTON 0x07
 #define PIN_COIL_BUTTON 0x08
-#define PIN_JOYSTICK_X A0
-#define PIN_JOYSTICK_Y A1
+#define PIN_JOYSTICK_X 0xA0
+#define PIN_JOYSTICK_Y 0xA1
 
-#define TURN_SERVO 0x02
-#define TILT_SERVO 0x01
+#define SERVO_TURN 0x02
+#define SERVO_TILT 0x01
 
 #define JOYSTICK_DEADZONE_MIN 450
 #define JOYSTICK_DEADZONE_MAX 562
 
-#define DELAY_OPEN_CLOSE_VALVE 175
+#define DELAY_OPEN_CLOSE_VALVE 275
 #define DELAY_CLOSE_VALVE_FIRE 200
 #define DELAY_FIRE_STOP_FIRE 370
 #define DELAY_RE_AUTO_FIRE 380
 
 #define RIGHT 1
 
-bool valveOpen = false;
-bool firing = false;
+bool loading;
+bool firing;
+bool autoFiring;
 
-bool autoFiring = false;
 Timer autoFireTimer;
 
 void joystickToServo(double joystickValue, int servo) {
@@ -38,36 +38,47 @@ void joystickToServo(double joystickValue, int servo) {
     }
 }
 
-void openValve() {
-    if (!valveOpen) {
-        digitalWrite(PIN_VALVE, HIGH);
-        valveOpen = true;
+void joystickToTurn(double joystickValue, int servo) {
+    if (joystickValue >= JOYSTICK_DEADZONE_MAX) {
+        Dynamixel.turn(servo, RIGHT, map(joystickValue, 512, 1023, 0, 511));
+    } else if (joystickValue < JOYSTICK_DEADZONE_MIN) {
+        Dynamixel.turn(servo, LEFT, map(joystickValue, 512, 0, 0, 511));
+    } else {
+        Dynamixel.turn(servo, RIGHT, 0);
     }
 }
 
-void closeValve() {
-    if (valveOpen) {
-        digitalWrite(PIN_VALVE, LOW);
-        valveOpen = false;
+void load() {
+    if (!loading) {
+        digitalWrite(PIN_COIL, LOW);
+        loading = true;
+    }
+}
+
+void stopLoad() {
+    if (loading) {
+        digitalWrite(PIN_COIL, HIGH);
+        loading = false;
     }
 }
 
 void fire() {
     if (!firing) {
-        digitalWrite(PIN_COIL, HIGH);
+        digitalWrite(PIN_VALVE, LOW);
         firing = true;
     }
 }
 
 void stopFire() {
     if (firing) {
-        digitalWrite(PIN_COIL, LOW);
+        digitalWrite(PIN_VALVE, HIGH);
         firing = false;
     }
 }
 
 void autoFire() {
-    autoFireTimer.after(DELAY_OPEN_CLOSE_VALVE, &closeValve);
+    load();
+    autoFireTimer.after(DELAY_OPEN_CLOSE_VALVE, &stopLoad);
     autoFireTimer.after(DELAY_CLOSE_VALVE_FIRE, &fire);
     autoFireTimer.after(DELAY_FIRE_STOP_FIRE, &stopFire);
     autoFireTimer.after(DELAY_RE_AUTO_FIRE, &stopAutoFire);
@@ -87,37 +98,39 @@ void setup() {
     pinMode(PIN_COIL_BUTTON, INPUT);
   
     Dynamixel.begin(1000000, PIN_SERVO_DIRECTION);
-    Dynamixel.setEndless(TURN_SERVO, ON);
-    Dynamixel.setEndless(TILT_SERVO, ON);
+    Dynamixel.setEndless(SERVO_TURN, ON);
+    Dynamixel.setEndless(SERVO_TILT, ON);
 
-    for (int i = 0; i < 14; ++i) {
-        digitalWrite(i, LOW);
-    }
+    loading = false;
+    firing = false;
+    autoFiring = false;
+
+    digitalWrite(PIN_COIL, HIGH);
 }
 
 void loop() {  
-    joystickToServo(analogRead(PIN_JOYSTICK_X), TURN_SERVO);
-    joystickToServo(analogRead(PIN_JOYSTICK_Y), TILT_SERVO);
-    
+    joystickToTurn(analogRead(PIN_JOYSTICK_X), SERVO_TURN);
+    joystickToServo(analogRead(PIN_JOYSTICK_Y), SERVO_TILT);
+
     if (autoFiring) {
         autoFireTimer.update();
         return;
     }
-
+    
     if (digitalRead(PIN_VALVE_BUTTON) == HIGH && digitalRead(PIN_COIL_BUTTON)) {
-        autoFire();
         autoFiring = true;
-    }
-
-    if (digitalRead(PIN_VALVE_BUTTON) == HIGH) {
-        openValve(); 
-    } else {
-        closeValve();
+        autoFire();
     }
 
     if (digitalRead(PIN_COIL_BUTTON) == HIGH) {
-        fire();
+        load(); 
     } else {
+        stopLoad();
+    }
+
+    if (digitalRead(PIN_VALVE_BUTTON) == HIGH) {
         stopFire();
+    } else {
+        fire();
     }
 } 
